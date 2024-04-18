d3f906740c9ae8eeae68f80b82fce54e12455a1e
Monika Kushwaha <monikakushwaha2001@gmail.com>
2024-04-18 09:52:08 +0530

    Add project files.

---
+++ MVCAppUsingAjax/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;//For using LINQ to XML
+
+namespace MVCAppUsingAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string GetScore()
+        {
+            // Get the physical path of the file for the given virtual path
+            string physicalPath = Server.MapPath("~/matches/Score.xml");
+
+            //Loads the XML file into the application from the specified location
+            var doc = XElement.Load(physicalPath);
+
+            //Reads the value or content of "score" element from the xml file which is loaded
+            string score = doc.Element("Score").Value;
+            return score;
+        }
+    }
+}
---
+++ MVCAppUsingAjax/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using MVCAppUsingAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAppUsingAjax.Controllers
+{
+    public class ProductController : Controller
+    {
+        NorthwindEntities dc = new NorthwindEntities();
+        public ViewResult DisplayProducts()
+        {
+            return View(dc.Products);
+        }
+        [HttpPost]
+        public ViewResult SearchProduct(string SearchTerm)
+        {
+            List<Product> Products = new List<Product>();
+            if (SearchTerm.Trim().Length > 0)
+            {
+                Products = dc.Products.ToList();
+            }
+            else
+            {
+                //You can implement the Query in any of the below 2 years.
+                Products = (from P in dc.Products where P.ProductName.Contains(SearchTerm) select P).ToList();
+
+                Products=dc.Products.Where(P => P.ProductName.Contains(SearchTerm)).ToList();
+
+            }
+            return View("DisplayProducts", Products);
+        }
+        public JsonResult GetProducts(string term)
+        {
+            List<string> Products = dc.Products.Where(P => P.ProductName.StartsWith(term)).Select(P => P.ProductName).ToList();
+            return Json(Products, JsonRequestBehavior.AllowGet);
+        } 
+    }
+}
